fac2dfb328c6dfcd7b89e7223ddefc1cec753127
Kamichita san <satoushogo1@gmail.com>
2025-03-06 16:11:57 +0900

    Update v7path.py

--- v7path.py
+++ v7path.py
@@ -3,8 +3,8 @@ import os
 BOTTOKEN = os.getenv("DISCORD_TOKEN")
 CLIENT_ID = "1347091568552841226"
 CLIENT_SECRET = "Cf3y2WcY4rJKtXfxE6LIYPKiuG1zTOiy"
-REDIRECT_URI = "Flaskサーバーが建てられている場所(IPアドレスやドメイン)をここに"
+REDIRECT_URI = "https://backup-production-0f5f.up.railway.app/"
 
 usadata_path = "userdata.json"
 serverdata_folder_path = "/server/"
-authurl = "作ったURLをここに"
+authurl = "https://discord.com/oauth2/authorize?client_id=1347091568552841226&response_type=code&redirect_uri=https%3A%2F%2Fbackup-production-0f5f.up.railway.app%2F&scope=identify+guilds.join"
