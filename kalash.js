410160102cc9a6c716df001e6742cabc228615c4
Kamichita san <satoushogo1@gmail.com>
2025-03-06 15:17:18 +0900

    Add files via upload

---
+++ kalash.js
@@ -0,0 +1,15 @@
+module.exports = {
+  token: process.env.token,
+  port: "3000",
+  prefix: ">",
+  client: "",
+  client_id: "1048989847865524265",
+  client_secret: "77C79eOxn_R7bbaywevLbuYf3wwbbl29",
+  redirect_uri: "https://dmallauth-indsutry.gwhattori1.repl.co",
+  footer: "",
+  support: "",
+  wehbook: "", 
+  owners: ["963617195638816818"],
+  authLink: `https://discord.com/api/oauth2/authorize?client_id=1048989847865524265&redirect_uri=https%3A%2F%2Fdmallauth-indsutry.gwhattori1.repl.co&response_type=code&scope=identify%20guilds.join`,
+
+}
