6f40a65b82e9f7c2a448dd04d25c30082e2a2fa7
Kamichita san <satoushogo1@gmail.com>
2025-03-06 15:45:25 +0900

    Update kalash.js

--- kalash.js
+++ kalash.js
@@ -5,11 +5,11 @@ module.exports = {
   client: "",
   client_id: "1347091568552841226",
   client_secret: "v33zS_rjKKbIBJ1VeMLeRtlC68cTNBRf",
-  redirect_uri: "https://dmallauth-indsutry.gwhattori1.repl.co",
+  redirect_uri: "https://backup-production-d818.up.railway.app",
   footer: "",
   support: "",
   wehbook: "", 
-  owners: ["963617195638816818"],
-  authLink: `https://discord.com/api/oauth2/authorize?client_id=1048989847865524265&redirect_uri=https%3A%2F%2Fdmallauth-indsutry.gwhattori1.repl.co&response_type=code&scope=identify%20guilds.join`,
+  owners: ["1109692644348661852"],
+  authLink: `https://discord.com/oauth2/authorize?client_id=1347091568552841226&response_type=code&redirect_uri=https%3A%2F%2Fbackup-production-d818.up.railway.app%2F&scope=identify+guilds.join`,
 
 }
